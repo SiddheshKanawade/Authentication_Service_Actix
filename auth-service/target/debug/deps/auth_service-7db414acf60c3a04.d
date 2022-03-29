186da3a86418caf55b8e561b05e0924b687c903c
macbook air <siddhesh.kanawade@iitgn.ac.in>
2022-03-30 02:09:06 +0530

    added database

---
+++ auth-service/target/debug/deps/auth_service-7db414acf60c3a04.d
@@ -0,0 +1,7 @@
+/Users/macbookair/Desktop/Siddhesh/Authentication Service using Actix/Authentication_Service_Actix/auth-service/target/debug/deps/auth_service-7db414acf60c3a04: src/main.rs src/config/mod.rs src/handlers/mod.rs
+
+/Users/macbookair/Desktop/Siddhesh/Authentication Service using Actix/Authentication_Service_Actix/auth-service/target/debug/deps/auth_service-7db414acf60c3a04.d: src/main.rs src/config/mod.rs src/handlers/mod.rs
+
+src/main.rs:
+src/config/mod.rs:
+src/handlers/mod.rs:
---
+++ auth-service/target/rls/debug/deps/auth_service-0c06e5e92ebfbf91.d
@@ -0,0 +1,7 @@
+/Users/macbookair/Desktop/Siddhesh/Authentication Service using Actix/Authentication_Service_Actix/auth-service/target/rls/debug/deps/auth_service-0c06e5e92ebfbf91.rmeta: src/main.rs src/config/mod.rs src/handlers/mod.rs
+
+/Users/macbookair/Desktop/Siddhesh/Authentication Service using Actix/Authentication_Service_Actix/auth-service/target/rls/debug/deps/auth_service-0c06e5e92ebfbf91.d: src/main.rs src/config/mod.rs src/handlers/mod.rs
+
+src/main.rs:
+src/config/mod.rs:
+src/handlers/mod.rs:
